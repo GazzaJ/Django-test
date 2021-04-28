48e7da7184966516dbc2a9ba6430ed0e3f748132
GazzaJ <johnge71@gmail.com>
2021-04-28 14:47:52 +0000

    Troubleshhoting bugs with local dev environment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-%19=4d#*)4^fjxs1!dmql
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
 
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+if development:
+    ALLOWED_HOSTS = ['localhost']
+else:
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
@@ -74,8 +77,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_todo.wsgi.application'
 
-#Database
-#https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 if development:
     DATABASES = {
